Fixed unit conversion bug

algorithms/amplitude_estimators/mod_iae.py
@@ -373,7 +373,8 @@ class ModifiedIterativeAmplitudeEstimation(AmplitudeEstimator):
                     self._quantum_instance._run_config.shots = N - round_shots
                     print()
                     print('shots_i_max:', shots_i_max)
-                    print('N-round_shots:', N - round_shots)
+                    print('N:', N)
+                    print('N - round_shots:', N - round_shots)
                     round_shots = N
                 
                     ## run measurements for Q^k A|0> circuit
@@ -395,6 +396,8 @@ class ModifiedIterativeAmplitudeEstimation(AmplitudeEstimator):
                     
                     one_counts_total += one_counts
                     prob = one_counts_total / N
+
+                    print('prob:', prob)
                     
                     ##
                 
@@ -407,9 +410,11 @@ class ModifiedIterativeAmplitudeEstimation(AmplitudeEstimator):
                         )
                     
                     R_i = int(K * theta_intervals[-1][0])
+                    print("R_i:", R_i)
 #                 R_equal = int(4 * K * theta_intervals[-1][0]) == int(4 * K * theta_intervals[-1][1])
                     q_i = (R_i % 4) + 1
 
+                    print()
                     # compute theta_i_min, theta_i_max
                     if q_i == 1:
                         theta_i_min = np.arcsin(np.sqrt(a_i_min))
@@ -432,6 +437,8 @@ class ModifiedIterativeAmplitudeEstimation(AmplitudeEstimator):
                         
                     theta_i_min /= (np.pi / 2)
                     theta_i_max /= (np.pi / 2)
+                    theta_i_min -= (q_i - 1)
+                    theta_i_max -= (q_i - 1)
                     
                     # compute theta_u, theta_l of this iteration
                     theta_l = (R_i + theta_i_min) / K
@@ -440,10 +447,12 @@ class ModifiedIterativeAmplitudeEstimation(AmplitudeEstimator):
                     theta_intervals.append([theta_l, theta_u])
 
                     # compute a_u_i, a_l_i
-                    a_l = float(np.square(np.sin(theta_l)))
-                    a_u = float(np.square(np.sin(theta_u)))
+                    a_l = float(np.square(np.sin(np.pi * theta_l / 2)))
+                    a_u = float(np.square(np.sin(np.pi * theta_u / 2)))
                     a_intervals.append([a_l, a_u])
-                    
+
+                    print('a i interval:', [a_i_min, a_i_max])
+                    print('theta i interval:', [theta_i_min, theta_i_max])
                     print('theta interval:', [theta_l, theta_u])
                     print('a interval:', [a_l, a_u])
                     